dfc4e26d470b6eca643be9efb149cede7856a929
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2018-05-24 16:17:20 -0500

    MVP-3: add prod backend build

--- bundleCode.webpack.js
+++ bundleCode.webpack.js
@@ -3,6 +3,7 @@
 
 var path = require('path');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 var babelOptions = {
   /*"presets": [
@@ -16,32 +17,44 @@ var babelOptions = {
   ]*/
 };
 
-module.exports = {
-  cache: true,
-  entry: './src/backend/index.ts',
-  output: {
-    path: path.resolve(__dirname, './dist/bundleCode'),
-    filename: 'index.js',
-    library: 'mule-backgammon',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      exclude: [/node_modules/, /src\/(frontend|mock-sdk)/],
-      use: [
-        {
-          loader: 'babel-loader',
-          options: babelOptions
-        },
-        {
-          loader: 'ts-loader'
-        }
-      ]
-    }]
-  },
-  plugins: [],
-  resolve: {
-    extensions: ['.ts']
-  },
+module.exports = (env) => {
+  var isProd = env.prod;
+
+  var plugins = [];
+
+  if (isProd) {
+    plugins.push(
+      new UglifyJsPlugin()
+    )
+  }
+
+  return {
+    cache: true,
+    entry: './src/backend/index.ts',
+    output: {
+      path: path.resolve(__dirname, './dist/bundleCode'),
+      filename: 'index.js',
+      library: 'mule-backgammon',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        exclude: [/node_modules/, /src\/(frontend|mock-sdk)/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }]
+    },
+    plugins: plugins,
+    resolve: {
+      extensions: ['.ts']
+    },
+  };
 };
